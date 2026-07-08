d2153021cdc840cbd6518cd8df2627332a09ae5c
Mai Quoc Viet <duvimuahe123@gmail.com>
2026-07-08 12:16:41 +0700

    Fix sitemap domain to vstep.vietdon.vn on every boot

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ set -o errexit
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py loaddata data.json
+python manage.py shell -c "from django.contrib.sites.models import Site; Site.objects.update_or_create(id=1, defaults={'domain': 'vstep.vietdon.vn', 'name': 'VSTEP Master'})"
 exec gunicorn vstep.wsgi:application
